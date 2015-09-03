f03d5f5ca81cb6c8d7e1c97b4ce673002e4a377e
leandrovianna <leandrovianna50@gmail.com>
2015-09-03 12:20:21 -0300

    Corrigindo alguns erros no ComunicacaoDAO

--- app/src/main/java/com/inspector/persistencia/ComunicacaoSPDao.java
+++ app/src/main/java/com/inspector/persistencia/ComunicacaoSPDao.java
@@ -8,14 +8,15 @@ import com.inspector.persistencia.dao.ComunicacaoDAO;
 import com.inspector.util.App;
 
 import java.sql.Timestamp;
+import java.util.Calendar;
 
 /**
  * Created by leandro on 02/09/15.
  */
 public class ComunicacaoSPDao implements ComunicacaoDAO {
 
-    public static final String COMUNICACAO_LAST_UPDATE = "Comunicacao.last_update";
-    public static final String COMUNICACAO_TOKEN = "Comunicacao.token";
+    private static final String COMUNICACAO_LAST_UPDATE = "Comunicacao.last_update";
+    private static final String COMUNICACAO_TOKEN = "Comunicacao.token";
     private SharedPreferences sharedPreferences;
 
     private SharedPreferences getSharedPreferences() {
@@ -32,16 +33,21 @@ public class ComunicacaoSPDao implements ComunicacaoDAO {
             throw new IllegalArgumentException("Token is empty");
 
         getSharedPreferences().edit()
-                .putString(COMUNICACAO_LAST_UPDATE, comunicacao.getLast_update().toString());
+                .putString(COMUNICACAO_LAST_UPDATE, comunicacao.getLast_update().toString())
+                .commit();
+
         getSharedPreferences().edit()
-                .putString(COMUNICACAO_TOKEN, comunicacao.getToken());
+                .putString(COMUNICACAO_TOKEN, comunicacao.getToken())
+                .commit();
     }
 
     @Override
     public Comunicacao get() {
 
         Comunicacao c = new Comunicacao();
-        Timestamp lastUpdate = Timestamp.valueOf(getSharedPreferences().getString(COMUNICACAO_LAST_UPDATE, "0"));
+        Timestamp lastUpdate = Timestamp.valueOf(
+                getSharedPreferences().getString(COMUNICACAO_LAST_UPDATE,
+                        new Timestamp(Calendar.getInstance().getTimeInMillis()).toString()));
 
         c.setLast_update(lastUpdate);
         c.setToken(getSharedPreferences().getString(COMUNICACAO_TOKEN, ""));
