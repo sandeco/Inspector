8c28ddee41e1336cbfd7bc144e54d01f7c9e173b
leandrovianna <leandrovianna50@gmail.com>
2015-09-10 20:31:27 -0300

    Melhorando a estrutura de importação

--- app/src/main/java/com/inspector/newimport/ImportDataService.java
+++ app/src/main/java/com/inspector/newimport/ImportDataService.java
@@ -24,39 +24,21 @@ public class ImportDataService extends Service implements ImportDataTask.Listene
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        Log.i(TAG, "Ativado");
+        Log.i(TAG, "Started " + intent.toString());
 
-        //solicitando que sincronize os dados com o servidor
-        importDataTask.sync();
+        //executar a sincronização a cada 20 segundos
+        importDataTask.syncEachSeconds(20);
 
-        return super.onStartCommand(intent, flags, startId);
+        return START_REDELIVER_INTENT;
     }
 
     @Override
     public void update(boolean success) {
 
-        //codigo abaixo para testar o fluxo entre o service e a importadora
-
         if (success)
             Log.i(TAG, "Sincronização completada");
         else
             Log.i(TAG, "Sincronização falhou");
-
-        //TODO: Código para pausar a operação por 20 segundos
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(20000);
-
-                    importDataTask.sync(); //solicitando o sincronismo novamente
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        thread.start();
     }
 
     @Override
@@ -69,6 +51,8 @@ public class ImportDataService extends Service implements ImportDataTask.Listene
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        importDataTask.stopSync();
 
         Log.i(TAG, "Sendo destruído (onDestroy)");
     }
--- app/src/main/java/com/inspector/newimport/ImportDataTask.java
+++ app/src/main/java/com/inspector/newimport/ImportDataTask.java
@@ -15,31 +15,37 @@ import com.inspector.util.App;
 
 import org.json.JSONArray;
 
-/**
- * Created by leandro on 10/09/15.
- */
-public class ImportDataTask {
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ImportDataTask implements Runnable {
     private static final String TAG = "ImportDataTask";
 
-    public interface Listener {
-        void update(boolean success);
-    }
-
     private RequestQueue mQueue;
-    private Listener listener;
+    private Listener mListener;
+    private ScheduledThreadPoolExecutor mThreadPool;
+    private boolean mRunning;
 
     public ImportDataTask(Listener listener) {
         mQueue = Volley.newRequestQueue(App.getContext());
-        this.listener = listener;
+        mThreadPool = new ScheduledThreadPoolExecutor(1);
+        this.mListener = listener;
+        this.mRunning = false;
+    }
+
+    @Override
+    public void run() {
+        if (mRunning)
+            downloadNewData();
     }
 
     /**
-     * Sincroniza os dados com servidor e ao final notifica o listener registrado
+     * Sincroniza os dados com servidor e ao final notifica o mListener registrado
      */
-    public void sync() {
+    private void downloadNewData() {
 
         if (!InternetCheck.isConnected(App.getContext()))
-            listener.update(false);
+            mListener.update(false);
         else {
 
             String url = PreferenceManager.getDefaultSharedPreferences(App.getContext())
@@ -52,18 +58,44 @@ public class ImportDataTask {
                         @Override
                         public void onResponse(JSONArray response) {
                             Log.v(TAG, response.toString());
-                            listener.update(true);
+                            mListener.update(true);
                         }
                     }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
                             Log.v(TAG, error.toString());
-                            listener.update(false);
+                            mListener.update(false);
                         }
                     });
 
             mQueue.add(request);
             mQueue.start();
         }
+    }
+
+    /**
+     * Executa periodicamente a sincronização de dados no intervalo de segundos passado
+     * @param seconds Segundo entre cada tentativa de sincronização
+     * @throws UnsupportedOperationException Disparada quando as tentativas de sincronismo já foram ativadas.
+     */
+    public void syncEachSeconds(int seconds) {
+
+        if (mRunning) {
+            throw new UnsupportedOperationException("Already running various sync attempts, stop them with stopSync()");
+        } else {
+            mThreadPool.scheduleAtFixedRate(this, 0, seconds, TimeUnit.SECONDS);
+            mRunning = true;
+        }
+    }
+
+    /**
+     * Parar o ciclo de tentativas de sincronismo
+     */
+    public void stopSync() {
+        mRunning = false;
+    }
+
+    public interface Listener {
+        void update(boolean success);
     }
 }
