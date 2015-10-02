ad2d659115eb899e3b1f6620f847de35519d252c
leandrovianna <leandrovianna50@gmail.com>
2015-10-02 18:03:29 -0300

    Atualizando activity de importação para novo sistema

--- app/src/main/java/com/inspector/activity/ImportarDadosActivity.java
+++ app/src/main/java/com/inspector/activity/ImportarDadosActivity.java
@@ -1,81 +1,89 @@
 package com.inspector.activity;
 
-import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ImportarDadosActivity extends ActionBarActivity{
+import com.inspector.R;
+import com.inspector.communication.importData.ProxyRest;
+import com.inspector.httpClient.InternetCheck;
 
-    //TODO: Atualizar para nova importação
+public class ImportarDadosActivity extends AppCompatActivity implements ProxyRest.Listener {
 
-//    private ProgressBar progressBar;
-//    private TextView tvAguarde;
-//    private Button btImportar;
-//
-//    private ImportadoraDados importadoraDados;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_importar_dados);
-//
-//        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-//        tvAguarde = (TextView) findViewById(R.id.tv_aguarde);
-//        btImportar = (Button) findViewById(R.id.bt_importar);
-//
-//        importadoraDados = new ImportadoraDados(this, this);
-//
-//        ativarTelaNormal();
-//    }
-//
-//    public void loadFromServer(View v) {
-//
-//        if (InternetCheck.isConnected(this)) {
-//            ativarTelaCarregamento();
-//            importadoraDados.iniciar();
-//            tvAguarde.setText("Os dados estão sendo importados para o seu dispositivo");
-//        } else {
-//            Toast.makeText(this, getString(R.string.internet_erro), Toast.LENGTH_LONG).show();
-//        }
-//    }
-//
-//    @Override
-//    public void updateImportacao(boolean result) {
-//        if (result)
-//            tvAguarde.setText(getString(R.string.dados_importadosEgravados));
-//        else
-//            tvAguarde.setText(getString(R.string.dados_naoGravados));
-//
-//        ativarTelaNormal();
-//        tvAguarde.setVisibility(View.VISIBLE);
-//    }
-//
-//    private void ativarTelaCarregamento() {
-//        btImportar.setVisibility(View.INVISIBLE);
-//        progressBar.setVisibility(View.VISIBLE);
-//        tvAguarde.setVisibility(View.VISIBLE);
-//    }
-//
-//    private void ativarTelaNormal() {
-//        btImportar.setVisibility(View.VISIBLE);
-//        progressBar.setVisibility(View.INVISIBLE);
-//        tvAguarde.setVisibility(View.INVISIBLE);
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.importar_dados, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+    private ProgressBar progressBar;
+    private TextView tvAguarde;
+    private Button btImportar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_importar_dados);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        tvAguarde = (TextView) findViewById(R.id.tv_aguarde);
+        btImportar = (Button) findViewById(R.id.bt_importar);
+
+        ativarTelaNormal();
+    }
+
+    public void loadFromServer(View v) {
+
+        if (InternetCheck.isConnected()) {
+            ativarTelaCarregamento();
+
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    ProxyRest proxyRest = new ProxyRest();
+                    proxyRest.registerListener(ImportarDadosActivity.this);
+                    proxyRest.sync();
+                }
+            });
+
+            t.start();
+
+            tvAguarde.setText("Os dados estão sendo importados para o seu dispositivo");
+        } else {
+            Toast.makeText(this, getString(R.string.internet_erro), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void ativarTelaCarregamento() {
+        btImportar.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+        tvAguarde.setVisibility(View.VISIBLE);
+    }
+
+    private void ativarTelaNormal() {
+        btImportar.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
+        tvAguarde.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onError(Exception e) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ativarTelaNormal();
+                tvAguarde.setText(getString(R.string.dados_naoGravados));
+                tvAguarde.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    @Override
+    public void onSuccess() {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                ativarTelaNormal();
+                tvAguarde.setText(getString(R.string.dados_importadosEgravados));
+                tvAguarde.setVisibility(View.VISIBLE);
+            }
+        });
+    }
 }
